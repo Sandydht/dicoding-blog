0e7828e1a2069fe39978d1704354639fcd2b74e2
Sandydht <sandydwi75@gmail.com>
2023-09-01 23:06:35 +0700

    feat: set up dockerfile

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,15 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   mode: 'development',
   devServer: {
     port: 9000,
     open: true
-  }
+  },
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ],
 });
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,5 +2,32 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  optimization: {
+    moduleIds: 'hashed',
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  }
 });
